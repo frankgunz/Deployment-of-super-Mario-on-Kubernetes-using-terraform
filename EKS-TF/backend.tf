609e75e0c1a3684a3087511c54a2912ed543c5c7
frankgunz <franktsopmo@gmail.com>
2024-01-22 23:37:17 -0600

    modifying s3 bucket config

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket    = "geterraform"
-    key       = "vpc/jupiter_website-ecs.tfstate"
+    key       = "Mario"
     region    = "us-east-1"
     profile   = "terraform-user"
     dynamodb_table = "terraform-state-lock"
